18a0625f2280ed7b9546c93f22ee74c5eb1d21b3
Barbara Washington <barbara.m.washington@gmail.com>
2018-09-18 23:41:02 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resource :secrets, only: [:show]
- 
+  get '/login' => 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
